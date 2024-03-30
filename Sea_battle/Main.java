2b86cc13ebd3aaabbc8deae6bc4370785731f041
GlebKabaev <112932953+GlebKabaev@users.noreply.github.com>
2024-03-30 14:27:15 +0300

    Update Main.java

--- Sea_battle/Main.java
+++ Sea_battle/Main.java
@@ -1,5 +1,7 @@
 import pack.SimpleDotCom;
+import pack.GameHelper;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,13 +13,17 @@ public class Main {
         String move;
         int numOfMoves=0;
         SimpleDotCom dot=new SimpleDotCom();
+        ArrayList<String>loc2=new ArrayList<String>();
         int[]loc=new int[3];
         loc[0]=random.nextInt(7);
         loc[1]=loc[0]+1;
         loc[2]=loc[1]+1;
-        dot.setLocationCells(loc);
+        loc2.add(Integer.toString(loc[0]));
+        loc2.add(Integer.toString(loc[1]));
+        loc2.add(Integer.toString(loc[2]));
+        dot.setLocationCells(loc2);
         boolean isAlive=true;
-        while (isAlive==true){
+        while (isAlive){
             move=cin.nextLine();
             if(dot.checkYourself(move)=="Shipwreck"){
                 isAlive=false;
