ef7eb742626f9ba3c941300a72c6bdf28e96e69d
GlebKabaev <112932953+GlebKabaev@users.noreply.github.com>
2024-03-30 14:27:53 +0300

    Update SimpleDotCom.java

--- Sea_battle/SimpleDotCom.java
+++ Sea_battle/SimpleDotCom.java
@@ -1,29 +1,31 @@
 package pack;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SimpleDotCom {
 
-    int[] location;
+
+   private ArrayList<String> locationCells;
     int numOfHits=0;
     String result="Miss";
-    public void setLocationCells(int[] loc){
-        location=loc;
+    public void setLocationCells(ArrayList<String> loc){
+        locationCells=loc;
     }
-    public String checkYourself(String guess){
-        int guessInt=Integer.parseInt(guess);
-        for (int cell : location) {
-            if (cell == guessInt) {
-                numOfHits++;
-                if (numOfHits == 3) {
-                    result="Shipwreck";
-                    System.out.println(result);
-                    return result;
-                } else {
-                    result="Hit";
-                    System.out.println(result);
-                    return result;
-                }
+    public String checkYourself(String userInput){
+        int index=locationCells.indexOf(userInput);
+        if(index>=0)
+        {
+            locationCells.remove(index);
+            if (locationCells.isEmpty()){
+                result="Shipwreck";
+                System.out.println(result);
+                return result;
+            }
+            else {
+                result="Hit";
+                System.out.println(result);
+                return result;
             }
         }
         result="Miss";
