259d61db1001e4e42667c350b28714bf54989f35
GlebKabaev <112932953+GlebKabaev@users.noreply.github.com>
2024-05-04 11:28:52 +0300

    Update Grad.java

--- grad/pack/Grad.java
+++ grad/pack/Grad.java
@@ -19,41 +19,42 @@ public class Grad {
         return (x1*x1)+4*(x2*x2)+(x1*x2)+x1;
     }
     public  double deltafx1(double x1,double x2){
-        return 2*x1+x2;
+        return (2*x1)+x2;
     }
     public double deltafx2(double x1,double x2){
-        return 8*x2+x1;
+        return (8*x2)+x1;
     }
     public  double deltafxnorma(double x1,double x2){
         return Math.sqrt(Math.pow( deltafx1(x1,x2),2)+Math.pow( deltafx2(x1,x2),2));
     }
     public double normax(double[] arr1, double[] arr2){
-        return Math.sqrt(Math.pow(arr1[0]-arr2[0],2)+Math.pow(arr1[1]-arr2[2],2));
+        return Math.sqrt(Math.pow(arr1[0]-arr2[0],2)+Math.pow(arr1[1]-arr2[1],2));
     }
     public  double crnewx(double x,double deltax,double t){
-        return x-(deltax*t);
+        return x-(Math.abs( deltax*t));
     }
     public double rezult(double x1,double deltafx,double x2,double deltafx2){
-        System.out.println("0я итерация");
-        interval[0]=-3.0;
-        System.out.println("a="+interval[0]);
-        interval[1]=7.0;
-        System.out.println("b="+interval[1]);
+
+        interval[0]=-10000.0;
+
+        interval[1]=10000.0;
+
         el=0.5;
-        System.out.println("l="+el);
+
         flag2=true;
         Yk = interval[0]+(((3-Math.sqrt(5))/2)*(interval[1]-interval[0]));
-        System.out.println("Yk="+Yk);
+
         FYk = funk(x1,deltafx,x2,deltafx2,Yk);
-        System.out.println("F(Yk)="+FYk);
+
         Zk =interval[0]+interval[1]-Yk;
-        System.out.println("Zk="+Zk);
+
         FZk = funk(x1,deltafx,x2,deltafx2,Zk);
-        System.out.println("F(Zk)="+FZk);
+
+        x22=0;
+        i=0;
 
         while(flag2) {
             i++;
-            System.out.println(i + "я итерация");
             FYk = funk(x1,deltafx,x2,deltafx2,Yk);
             FZk = funk(x1,deltafx,x2,deltafx2,Zk);
             if (FYk <= FZk) {
@@ -70,14 +71,9 @@ public class Grad {
                 flag2 = false;
                 x22 = (interval[0] - interval[1]) / 2;
             }
-            System.out.println("a=" + interval[0]);
-            System.out.println("b=" + interval[1]);
-            System.out.println("Yk=" + Yk);
-            System.out.println("F(Yk)=" + FYk);
-            System.out.println("Zk=" + Zk);
-            System.out.println("F(Zk)=" + FZk);
+
             if (x22 != 0) {
-                System.out.println("x*=" + x22);
+
                 return x22;
             }
         }
@@ -94,10 +90,19 @@ public class Grad {
         while(flag){
             deltafx[0]=deltafx1(xnow[0],xnow[1]);
             deltafx[1]=deltafx2(xnow[0],xnow[1]);
+            System.out.printf("Delta f(x)= %.2f %.2f%n",deltafx[0],deltafx[1]);
             if(deltafxnorma(xnow[0],xnow[1])<eps1){
                 x[0]=xnow[0];
                 x[1]=xnow[1];
                 flag=false;
+                System.out.println("Прогоамма завершена по условию norma<eps1"+ x[0]+" "+x[1]);
+                break;
+            }
+            if(k>m){
+                x[0]=xnow[0];
+                x[1]=xnow[1];
+                flag=false;
+                System.out.println("Прогоамма завершена по условию k>m"+ x[0]+" "+x[1]);
                 break;
             }
            t=rezult(xnow[0],deltafx[0],xnow[1],deltafx[1]);
@@ -105,9 +110,19 @@ public class Grad {
             xbef[1]=xnow[0];
             xnow[0]=crnewx(xbef[0],deltafx[0],t);
             xnow[1]=crnewx(xbef[1],deltafx[1],t);
-            System.out.println(xnow[0]+" "+xnow[1]);
-            flag=false;
-
+            System.out.printf("X= %.10f %.10f%n",xnow[0],xnow[1]);
+            boolean req1=normax(xnow,xbef)<eps2;
+            System.out.println("норма равна "+normax(xnow,xbef));
+            boolean req2=(thisfunk(xnow[0],xnow[1])-thisfunk(xbef[0],xbef[1]))<eps2;
+            if(req2==req1 && req1){
+                x[0]=xnow[0];
+                x[1]=xnow[1];
+                flag=false;
+                System.out.println("Прогоамма завершена по условию norma<eps2 и разница функций<eps2 \n"+ x[0]+" "+x[1]);
+                break;
+            }else {
+                k++;
+            }
 
         }
     }
