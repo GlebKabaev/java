382081831d88c59fb82faa89b06bdcee5695d38c
GlebKabaev <112932953+GlebKabaev@users.noreply.github.com>
2024-05-15 14:13:08 +0300

    Update Flatcher_Rivs.java

--- Flatcher_Rivs.java
+++ Flatcher_Rivs.java
@@ -19,13 +19,15 @@ public class Flatcher_Rivs {
     private double x22;private double el;private int i;double t=0.14;double beta;
     public double thisfunk(double x1,double x2){
         return (x1*x1)+4*(x2*x2)+(x1*x2)+x1;
+        //return  2*(x1*x1)+(x1*x2)+(x2*x2);
     }
     public  double deltafx1(double x1,double x2){
-        return (2*x1)+x2;
+        return (4*x1)+x2;
     }
     public double deltafx2(double x1,double x2){
-        return (8*x2)+x1;
+        return x1+2*x2;
     }
+
     public double normaOfVektor(double first, double second){
         return Math.sqrt((first*first)+(second*second));
     }
@@ -51,7 +53,7 @@ public class Flatcher_Rivs {
 
         while(flag2) {
             i++;
-            System.out.println("i="+i);
+
             FYk = funk(x1,d,x2,d2,Yk);
             FZk = funk(x1,d,x2,d2,Zk);
             if (FYk <= FZk) {
@@ -83,6 +85,7 @@ public class Flatcher_Rivs {
     }
     private double funk(double x,double d,double x2,double d2,double x22){
         return Math.pow(x+(d*x22),2)+4*Math.pow(x2+(d2*x22),2)+(x+(d*x22))*(x2+(d2*x22))+(x+(d*x22)) ;
+
     }
     Flatcher_Rivs(){
         k++;
@@ -91,6 +94,7 @@ public class Flatcher_Rivs {
         while(flag){
             deltafx[0]=deltafx1(xnow[0],xnow[1]);
             deltafx[1]=deltafx2(xnow[0],xnow[1]);
+            System.out.printf("Delta f(x)= %.2f %.2f%n",deltafx[0],deltafx[1]);
             if(normaOfVektor(deltafx[0],deltafx[1])<eps1){
                 x[0]=xnow[0];
                 x[1]=xnow[1];
@@ -108,27 +112,30 @@ public class Flatcher_Rivs {
             if(k==0){
                 d[0]=-1*deltafx[0];
                 d[1]=-1*deltafx[1];
+                System.out.printf("d= %.2f %.2f%n",d[0],d[1]);
             }
             if(k>=1){
             beta=(Math.pow(normaOfVektor(deltafx[0],deltafx[1]),2))/
                     (Math.pow(normaOfVektor(deltafx1(xbef[0],xbef[1]),deltafx2(xbef[0],xbef[1])),2));
-            beta=Math.round(beta*100.0)/100.0;
                 dbef[0]=d[0];
                 dbef[1]=d[1];
-                d[0]=deltafx[0]+beta*dbef[0];
-                d[1]=deltafx[1]+beta*dbef[1];
+                d[0]=-1*(deltafx1(xbef[0],xbef[1]))+beta*dbef[0];
+                d[1]=(-1*deltafx2(xbef[0],xbef[1]))+beta*dbef[1];
+                System.out.printf("d= %.2f %.2f%n",d[0],d[1]);
             }
-            //t=Math.abs(rezult(xnow[0],d[0],xnow[1],d[1]));
+            t=Math.abs(rezult(xnow[0],d[0],xnow[1],d[1]));
+
             xbef[0]=xnow[0];
             xbef[1]=xnow[1];
-            xnow[0]=xbef[0]+(t*d[0]);
-            xnow[1]=xbef[1]+(t*d[1]);
+            xnow[0]=xbef[0]+(0.007*d[0]);
+            xnow[1]=xbef[1]+(0.007*d[1]);
+            System.out.printf("X= %.10f %.10f%n",xnow[0],xnow[1]);
             if( ((Math.sqrt(((xnow[0]-xbef[0])*(xnow[0]-xbef[0]))+((xnow[1]-xbef[1])*(xnow[1]-xbef[1]))))<eps2)&&
                     ((Math.abs(thisfunk(xnow[0],xnow[1])-thisfunk(xbef[0],xbef[1])))<eps2) ){
                 x[0]=xnow[0];
                 x[1]=xnow[1];
                 flag=false;
-                System.out.println("расчет окончен по норме разности x <eps2 и модуль разности функций <eps2 c результатом"+x[0]+" "+x[1]);
+                System.out.println("расчет окончен по норме разности x <eps2 и модуль разности функций <eps2 c результатом "+x[0]+" "+x[1]);
                 break;
             }
             k++;
